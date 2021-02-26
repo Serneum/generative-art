efa477f258b2d11175b42a3809d9ec46047a170d
Chris Rees <crees@covermymeds.com>
2021-02-26 13:30:00 -0500

    Add fading from top and bottom

--- dots/dots.go
+++ dots/dots.go
@@ -31,6 +31,7 @@ type Dots struct {
 	strokeSize        float64
 	initialStrokeSize float64
 	increment         int
+	currentAlpha      float64
 }
 
 func NewDots(source image.Image, userParams UserParams) *Dots {
@@ -60,11 +61,12 @@ func NewDots(source image.Image, userParams UserParams) *Dots {
 		s.AlphaIncrease = float64(totalIncrease) / (float64(xIter * yIter))
 	}
 
-	if s.Fade == "right" {
+	if s.Fade == "right" || s.Fade == "bottom" {
 		s.InitialAlpha = 255 - s.InitialAlpha
 		s.AlphaIncrease *= -1
 	}
 
+	s.currentAlpha = s.InitialAlpha
 	s.source = source
 	s.dc = canvas
 	return s
@@ -73,6 +75,9 @@ func NewDots(source image.Image, userParams UserParams) *Dots {
 func (s *Dots) Update() {
 	xOffset := float64(s.DestWidth-(int(s.DestWidth/s.increment)*s.increment)) / 2
 	yOffset := float64(s.DestHeight-(int(s.DestHeight/s.increment)*s.increment)) / 2
+
+	xIter := float64(s.sourceWidth) / float64(s.increment)
+	yIter := float64(s.sourceHeight) / float64(s.increment)
 
 	for i := s.Radius; i < s.sourceWidth; i += s.increment {
 		for j := s.Radius; j < s.sourceHeight; j += s.increment {
@@ -89,16 +94,30 @@ func (s *Dots) Update() {
 				destY += float64(util.RandRange(s.StrokeJitter))
 			}
 
-			s.dc.SetRGBA255(r, g, b, int(s.InitialAlpha))
+			if s.Fade == "random" {
+				s.currentAlpha = float64(rand.Intn(256))
+			} else if s.Fade != "none" {
+				row, col, iter := j, i, yIter
+				if s.Fade == "top" || s.Fade == "bottom" {
+					row, col, iter = i, j, xIter
+				}
+
+				gridX := col / (s.Radius * 2)
+				gridY := row / (s.Radius * 2)
+				iterationsBeforePos := float64(gridX)*iter + float64(gridY)
+
+				s.currentAlpha = s.InitialAlpha + iterationsBeforePos*s.AlphaIncrease
+				if s.currentAlpha > 255 {
+					s.currentAlpha = 255
+				} else if s.currentAlpha < 0 {
+					s.currentAlpha = 0
+				}
+			}
+
+			s.dc.SetRGBA255(r, g, b, int(s.currentAlpha))
 			s.dc.DrawCircle(destX, destY, float64(s.Radius))
 			s.dc.Fill()
 			s.dc.Stroke()
-
-			if s.Fade == "random" {
-				s.InitialAlpha = float64(rand.Intn(256))
-			} else if s.Fade != "none" {
-				s.InitialAlpha += s.AlphaIncrease
-			}
 		}
 	}
 }
